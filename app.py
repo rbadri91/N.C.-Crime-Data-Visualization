b66c1af1ff43be5467ff01c9107a59f640ac9555
badri <rbadri01@gmail.com>
2017-03-29 02:42:17 -0400

    Scatter plot

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 import matplotlib.pylab as plt
+from sklearn.preprocessing import StandardScaler
 from scipy.spatial.distance import cdist
 import collections
 from collections import defaultdict
@@ -27,7 +28,7 @@ FIELDS = {'county': True, 'year': True, 'crmrte': True, 'prbarr': True, 'prbconv
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html")   
 
 @app.route("/crime/projects")
 def crime_projects():
@@ -45,36 +46,36 @@ def crime_projects():
 
 proj_details=crime_projects();
 crime_data = pd.read_json(proj_details)
-print(crime_data)
+# print(crime_data)
 # testarray = ast.literal_eval(proj_details)
 clusterObj= crime_data[['county','year','crmrte','prbarr','prbconv','prbpris','avgsen',
 						'density','wcon','wtuc','wtrd','wfir','wser','wmfg','taxpc','pctmin','wfed','wsta','wloc','mix','pctymle']]
-clustervar=clusterObj.copy()
+clustervar=clusterObj.copy() 
 
-clustervar['county']= preprocessing.scale(clustervar['county'].astype('float64'))
-clustervar['year']= preprocessing.scale(clustervar['year'].astype('float64'))
-clustervar['crmrte']= preprocessing.scale(clustervar['crmrte'].astype('float64'))
-clustervar['prbarr']= preprocessing.scale(clustervar['prbarr'].astype('float64'))
-clustervar['prbconv']= preprocessing.scale(clustervar['prbconv'].astype('float64'))
-clustervar['prbpris']= preprocessing.scale(clustervar['prbpris'].astype('float64'))
-clustervar['avgsen']= preprocessing.scale(clustervar['avgsen'].astype('float64'))
-clustervar['density']= preprocessing.scale(clustervar['density'].astype('float64'))
-clustervar['wcon']= preprocessing.scale(clustervar['wcon'].astype('float64'))
-clustervar['wtuc']= preprocessing.scale(clustervar['wtuc'].astype('float64'))
-clustervar['wtrd']= preprocessing.scale(clustervar['wtrd'].astype('float64'))
-clustervar['wfir']= preprocessing.scale(clustervar['wfir'].astype('float64'))
-clustervar['wser']= preprocessing.scale(clustervar['wser'].astype('float64'))
-clustervar['wmfg']= preprocessing.scale(clustervar['wmfg'].astype('float64'))
-clustervar['taxpc']= preprocessing.scale(clustervar['taxpc'].astype('float64'))
-clustervar['pctmin']= preprocessing.scale(clustervar['pctmin'].astype('float64'))
-clustervar['wfed']= preprocessing.scale(clustervar['wfed'].astype('float64'))
-clustervar['wsta']= preprocessing.scale(clustervar['wsta'].astype('float64'))
-clustervar['wloc']= preprocessing.scale(clustervar['wloc'].astype('float64'))
-clustervar['mix']= preprocessing.scale(clustervar['mix'].astype('float64'))
-clustervar['pctymle']= preprocessing.scale(clustervar['pctymle'].astype('float64'))
+# clustervar['county']= preprocessing.scale(clustervar['county'].astype('float64'))
+# clustervar['year']= preprocessing.scale(clustervar['year'].astype('float64'))
+# clustervar['crmrte']= preprocessing.scale(clustervar['crmrte'].astype('float64'))
+# clustervar['prbarr']= preprocessing.scale(clustervar['prbarr'].astype('float64'))
+# clustervar['prbconv']= preprocessing.scale(clustervar['prbconv'].astype('float64'))
+# clustervar['prbpris']= preprocessing.scale(clustervar['prbpris'].astype('float64'))
+# clustervar['avgsen']= preprocessing.scale(clustervar['avgsen'].astype('float64'))
+# clustervar['density']= preprocessing.scale(clustervar['density'].astype('float64'))
+# clustervar['wcon']= preprocessing.scale(clustervar['wcon'].astype('float64'))
+# clustervar['wtuc']= preprocessing.scale(clustervar['wtuc'].astype('float64'))
+# clustervar['wtrd']= preprocessing.scale(clustervar['wtrd'].astype('float64'))
+# clustervar['wfir']= preprocessing.scale(clustervar['wfir'].astype('float64'))
+# clustervar['wser']= preprocessing.scale(clustervar['wser'].astype('float64'))
+# clustervar['wmfg']= preprocessing.scale(clustervar['wmfg'].astype('float64'))
+# clustervar['taxpc']= preprocessing.scale(clustervar['taxpc'].astype('float64'))
+# clustervar['pctmin']= preprocessing.scale(clustervar['pctmin'].astype('float64'))
+# clustervar['wfed']= preprocessing.scale(clustervar['wfed'].astype('float64'))
+# clustervar['wsta']= preprocessing.scale(clustervar['wsta'].astype('float64'))
+# clustervar['wloc']= preprocessing.scale(clustervar['wloc'].astype('float64'))
+# clustervar['mix']= preprocessing.scale(clustervar['mix'].astype('float64'))
+# clustervar['pctymle']= preprocessing.scale(clustervar['pctymle'].astype('float64'))   
 
 clus_train = clustervar
-# print(clus_train)
+print(clus_train.columns.values)
 def findSuitableK():
 	clusters=range(1,9) 
 	meandist=[]
@@ -85,7 +86,7 @@ def findSuitableK():
 	    meandist.append(sum(np.min(cdist(clus_train, model.cluster_centers_, 'euclidean'), axis=1))
 	    / clus_train.shape[0])
 	plt.plot(clusters, meandist)
-	plt.xlabel('Number of clusters')
+	plt.xlabel('Number of clusters') 
 	plt.ylabel('Average distance')
 	plt.title('Selecting k with the Elbow Method') # pick the fewest number of clusters that reduces the average distance    
 	plt.show()
@@ -133,40 +134,100 @@ def sampleClusters():
 
 sampled_dataFrame=sampleClusters()
 
+def randomSample():
+	# print(len(clus_train))
+	newClusterTrain= random.sample(crime_data.index,len(clus_train)//3)
+	print(newClusterTrain)
+	return newClusterTrain
+
+# newClusterFrame=randomSample()	
+
 pca = PCA(n_components=21)
-pca.fit(sampled_dataFrame)
-existing_2d = pca.transform(sampled_dataFrame)
+pca.fit(sampled_dataFrame)	
 # print(existing_2d)
-existing_df_2d = pd.DataFrame(existing_2d)
-existing_df_2d.index = sampled_dataFrame.index
-existing_df_2d.columns = ['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20','PC21']
+# existing_df_2d = pd.DataFrame(existing_2d)
+# existing_df_2d.index = sampled_dataFrame.index
+# existing_df_2d.columns = ['PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20','PC21']
 # existing_df_2d.columns = ['PC1','PC2','PC3']
-existing_df_2d.head()
+# existing_df_2d.head()
 # print(pca.get_covariance())
-# print(pca.components_)  
+loadings=pca.components_ 
+print(loadings[0])
+print(loadings[1])
 # print(pca.components_.T * math.sqrt(pca.explained_variance_))
 # print(pca.explained_variance_)
 # print(pca.explained_variance_ratio_.cumsum())
 
 def screeplot(pca, standardised_values):
     y = np.std(pca.transform(standardised_values), axis=0)**2
-    # print(y)
+    print("y here:",y) 
     x = np.arange(len(y)) + 1 
     # print(x)
     plt.plot(x, y, "o-") 
-    plt.xticks(x, ["PC"+str(i) for i in x], rotation=60)
+    plt.xticks(x, ["PC"+str(i) for i in x], rotation=60) 
     plt.ylabel("Variance")
     plt.show()
     return np.array(y),np.array(x)
     # showScreeplot()
 
-y,x =screeplot(pca, sampled_dataFrame)  
+y,x =screeplot(pca, sampled_dataFrame)   
 
 @app.route("/crime/screeplot")
 def showScreeplot():
-	# print(y.tolist())
-	return render_template("screePlot.html",y=y.tolist(),x=x.tolist())   
+	# print(y.tolist()) 
+	return render_template("screePlot.html",y=y.tolist(),x=x.tolist()) 
 
+def squaredLoadings():
+	w, h = 8, 21;
+	squaredLoadings = [0 for y in range(h)] 
+	for i in range(len(loadings)):
+		sum=0
+		for j in range(0,8):
+			sum = sum + loadings[i][j] **2
+		squaredLoadings[i]=sum	
+	return squaredLoadings
+
+sumSquareLoadings=squaredLoadings()
+
+
+@app.route("/crime/squaredLoadings")
+def showSqureloadingsPlot():
+	sortedSumSquareLoadings=sorted(sumSquareLoadings,reverse=True)
+	print(type(sortedSumSquareLoadings))
+	length= len(sortedSumSquareLoadings)
+	columns=[0 for y in range(length)] 
+	index=0
+	for i in sortedSumSquareLoadings: 
+		print(i)
+		columns[index]=clus_train.columns.values[sumSquareLoadings.index(i)]
+		index =index+1
+	print(columns)
+	return render_template("squaredloadings.html",y=sortedSumSquareLoadings,x=json.dumps(columns))
+
+def scatterPlot():
+	X_std = StandardScaler().fit_transform(sampled_dataFrame)
+	pcaComp= PCA(n_components=2)
+	yComp = pcaComp.fit_transform(X_std)
+	with plt.style.context('seaborn-whitegrid'):
+	    plt.figure(figsize=(6, 4))
+	    for lab, col in zip(('Cluster1', 'Cluster2', 'Cluster3'),
+	                        ('blue', 'red', 'green')):
+	        plt.scatter(yComp[y==lab, 0],
+	                    yComp[y==lab, 1],
+	                    label=lab,
+	                    c=col)
+	    plt.xlabel('Principal Component 1')
+	    plt.ylabel('Principal Component 2')
+	    plt.tight_layout()
+	    plt.show()
+
+# scatterPlot()
+
+@app.route("/crime/scatterPlot")
+def showScatterPlot():
+	xVal=np.around(loadings[0], decimals=4)
+	yVal=np.around(loadings[1], decimals=4)
+	return render_template("scatterPlot.html",y=yVal.tolist(),x=xVal.tolist())
 
 
 # def find_centers(X, K):
---
+++ js/screePlot.js
@@ -0,0 +1,75 @@
+  var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    padding=30,
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+  // var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+  //   y = d3.scaleLinear().rangeRound([height, 0]);
+  var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var xScale = d3.scale.ordinal()
+				.domain(d3.range(yVal.length))
+				.rangeRoundBands([0, width], 0.05);
+	var yScale = d3.scale.linear()
+				.domain([0, d3
+					.max(yVal)])
+				.range([height,0]);
+
+	xAxis = d3.svg.axis()
+			.scale(xScale)
+			.orient("bottom")
+			.ticks(13);
+
+	yAxis = d3.svg.axis()
+			.scale(yScale)
+			.orient("left")
+			.ticks(13);	
+
+	svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(" + 28 + "," + parseInt(parseInt(height) + parseInt(margin.top)+10) + ")");
+
+	svg.selectAll("g.x.axis").transition().duration(500).transition().duration(500).call(xAxis);
+
+	svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .attr("transform", "translate(" + 30 + "," + padding + ")")
+    .append("text")
+      .attr("transform", "rotate(-90)")	
+      .attr("y", 24)
+      .attr("dy", ".71em")		
+
+	
+	svg.selectAll("rect")
+   .data(yVal, function(d) { return d; })
+   .enter()
+   .append("rect")
+   .attr("x", function(d, i) {
+		return xScale(i)+padding;
+   })
+   .attr("y", function(d) {
+   	console.log("d here:")
+		return yScale(d)+margin.top;
+   })
+   .attr("width", xScale.rangeBand())
+   .attr("height", function(d) {
+		return height -yScale(d);
+   })
+   .attr("fill", function(d) {
+		return "rgb(0, 0, " + 255 + ")";
+   })
+   .on("mouseover", function(d){
+   			d3.select("#tooltip")
+					.style("left", d3.event.pageX + "px")
+					.style("top", d3.event.pageY + "px")
+					.style("opacity", 1)
+					.attr("zIndex", 10)
+					.select("#value")
+					.text(d);
+   })
+   .on("mouseout", function() {
+				d3.select("#tooltip")
+					.style("opacity", 0);
+	});	
